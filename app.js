4f9ee464732b2842947349fb95270087d2053b09
DraGoIlE <kavintito3@gmail.com>
2025-12-03 22:06:49 +0700

    webhook testing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("KENAPA SAYA AMBIL DEVOPS");
+  res.send("MINIKUBE TOLONG KASIH HAMBA JAWABAN");
 });
 
 app.listen(port, () => {
