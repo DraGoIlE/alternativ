7cf26fcdbc5e8c7458a9a0111ac5d24a79598f22
DraGoIlE <kavintito3@gmail.com>
2025-12-03 22:31:13 +0700

    please work

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("rey dominan besok - kata sodiq");
+  res.send("rey dominan besok - kata sodiq ok");
 });
 
 app.listen(port, () => {
