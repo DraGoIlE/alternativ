714cb8e97ba8077c7842933b15c6584e93832c8c
DraGoIlE <kavintito3@gmail.com>
2025-12-07 23:06:59 +0700

    test cicd from ngrok

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD with Jenkins and Kubernetes!");
+  res.send("NGROK WEBHOOK TESTING PART 2");
 });
 
 app.listen(port, () => {
