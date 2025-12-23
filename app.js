a7bfdc203b6c563401b371362d23497a990fb32a
DraGoIlE <kavintito3@gmail.com>
2025-12-24 01:47:40 +0700

    laporan

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Goodbye, jojo");
+  res.send("Goodbye, Jojo");
 });
 
 app.listen(port, () => {
